9d9419809513dbc17b0fa4bf1036b75e680f998f
m.wolanski <mateuszwolanski5@gmail.com>
2026-04-11 18:08:18 +0200

    init

---
+++ RobotFieldUi/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace RobotFieldUi.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
